3492aea279fb3128c42de94f8a039a2af8ae90c4
Mikhail <stv5458@gmail.com>
2025-01-03 01:06:20 +0300

    Feat: Add example

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
-<script setup lang="ts">
-</script>
-
 <template>
+	<div class="container mx-auto px-4 py-5">
+		<FormWrapper :options="formOptions" @on-form-submit="onFormSubmit">
+			<StringField :type="'email'" :name="'Email'" :placeholder="'name@company.com'" :icon="Mail"
+				:rules="{ email: true, isRequired: true }" />
+			<StringField :type="'password'" :name="'Password'" :placeholder="'••••••••'" :icon="Password"
+				:rules="{ minLength: 8, isRequired: true }" />
+			<div class="flex items-center justify-between">
+				<a href="#" class="link text-primary-500">Forgot password?</a>
+			</div>
+		</FormWrapper>
+	</div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import FormWrapper from '@/components/FormWrapper.vue';
+import StringField from '@/components/fields/StringField.vue';
+import Mail from '@/components/icons/MailIcon.vue';
+import Password from '@/components/icons/PasswordIcon.vue';
+import { FormProps } from '@/types/FormPropsTypes';
+
+const formOptions: FormProps = {
+	submitBtnText: 'Sign in'
+};
+
+const onFormSubmit = () => {
+
+}
+</script>
